f00c2d8aa9883a8ca0830170d274d0375b5739c8
OscarDiazMirai <oscar.diaz@mirai.com>
2026-07-07 15:05:47 +0200

    feat: create fn to get several styles CSS from source web with service worker and use it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Header from '@/layouts/header/Header.jsx'
 import Main from '@/layouts/main/Main.jsx'
 import '@/style.css'
+import {useCreateStylesSheetsCSS} from '@/core/core.js'
 
 const App = ()=>{
+  // Initialises communication with the extension and injects the three style sheets
+  useCreateStylesSheetsCSS(); 
+
   return(
     <>
     <Header/>
-    <Main/>    
+    <Main/>
+     <div className="contenedor-kit">
+      <button class="elementor-button">Example button</button>
+    </div>    
     </>
   )
 };
--- src/core/core.js
+++ src/core/core.js
@@ -1,30 +1,63 @@
+// archivo: core.js
 import { useEffect } from 'react';
 
-export const elementorKit = () => {
+// 
+export const useCreateStylesSheetsCSS = () => {
   useEffect(() => {
-    // console.log('[React] Ready component...');
-
     const getData = (event) => {
-      // console.log("[React] Receiving data...:", event.data);
-      // We check that the message comes from our extension and not from another script
       if (event.data && event.data.origin === "FROM_CONTENT_SCRIPT") {
-        console.log("¡ÉXITO EN REACT!", event.data);
+        console.log("¡SUCCESSFULL!", event.data);
+
+        const { elementorKit, styleCSS, engineCSS } = event.data;
+
+        // We create and inject the stylesheet for Elementor Kit
+        createCSSLinkTag("elementor-kit", elementorKit);
+
+        //  We create and inject the stylesheet for Style CSS
+        createCSSLinkTag("style-css", styleCSS);
+
+        //  We create and inject the stylesheet for Engine CSS
+        createCSSLinkTag("engine-css", engineCSS);
+
+        // Add to body class with elementor-kit-ID
+        addElementorKitId(event.data.elementorKitId);
       }
     };
-    // Run listener
+     // Get data from source web
     window.addEventListener("message", getData);
-
-    // We introduced a long delay (2 seconds) just to check whether it was an asynchronous loading issue
+    
     const timer = setTimeout(() => {
-      // console.log('[React] Enviando petición REQUEST_DATA_FROM_EXTENSION...');
       window.postMessage({ accion: "REQUEST_DATA_FROM_EXTENSION" }, "*");
     }, 2000);
 
-    // Clear listeners and timers
+    // Clean up event
     return () => {
       window.removeEventListener("message", getData);
       clearTimeout(timer);
     };
   }, []);
+};
 
+// Helper function for injecting plain text as an actual stylesheet into the browser
+const createCSSLinkTag = (id, contenidoCSS) => {
+  if (!contenidoCSS) return;
+
+  let elementoStyle = document.getElementById(id);
+
+  // If the tag is not present in the HEAD section, we create it
+  if (!elementoStyle) {
+    elementoStyle = document.createElement("style");
+    elementoStyle.id = id;
+    document.head.appendChild(elementoStyle);
+  }
+
+  // We apply the CSS in plain text format received from the extension
+  elementoStyle.textContent = contenidoCSS;
+};
+
+
+// Add elementor-kit-ID class to body to use any vars or classes
+const addElementorKitId = (id) => {
+  if (!document.body) return;
+  document.body.classList.add(`elementor-kit-${id}`)
 };
