cd3a96b428ecd9ad7bc84978499d85e23083ef0c
OscarDiazMirai <oscar.diaz@mirai.com>
2026-07-14 14:33:27 +0200

    mock: create mock data to test

---
+++ src/core/mock.js
@@ -0,0 +1,11 @@
+/* MOCKING DATA */
+const mockData = {
+    elementorKit: 'elementor Kit styles',
+    elementorKitId: "1397",
+    engineCSS: 'engine CSS styles',
+    origin: "FROM_CONTENT_SCRIPT",
+    styleCSS: "style CSS styles"
+}
+const { elementorKit, elementorKitId, engineCSS, styleCSS } = mockData;
+export default mockData
+
