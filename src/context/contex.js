ff0e5a8084d12cf196efc9a4e7c3932ec765418a
OscarDiazMirai <oscar.diaz@mirai.com>
2026-07-14 14:29:08 +0200

    feat: create context and provider

---
+++ src/context/contex.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+
+const DataStylesContext = createContext(null);
+export default DataStylesContext
---
+++ src/context/provider.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import DataStylesContext from './contex.js';
+
+const ServiceWorkerDataProvider = ({children}) => {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        const getData = (event) => {
+            if (event.data && event.data.origin === "FROM_CONTENT_SCRIPT") {
+                setData(event.data)
+            }else{
+                console.log('Error receiving data from the work service')
+            }
+        };
+        // Get data from source web
+        window.addEventListener("message", getData);
+
+        const timer = setTimeout(() => {
+            window.postMessage({ accion: "REQUEST_DATA_FROM_EXTENSION" }, "*");
+        }, 2000);
+
+        // Clean up event
+        return () => {
+            window.removeEventListener("message", getData);
+            clearTimeout(timer);
+        };
+    }, []);
+
+    return(
+        <DataStylesContext.Provider value={data}>
+            {children}
+        </DataStylesContext.Provider>
+    );
+};
+
+export default ServiceWorkerDataProvider;
