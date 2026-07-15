784206a7748ef1e4998aa3417c94df5e6e2968fd
OscarDiazMirai <oscar.diaz@mirai.com>
2026-07-15 11:06:39 +0200

    clean: clean up code

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { useContext } from 'react';
 import Header from '@/layouts/header/Header.jsx'
 import Main from '@/layouts/main/Main.jsx'
 import '@/style.css'
-// import {useGetDataFromServiceWorker} from '@/core/core.js'
 import CreateStyleTag from '@/components/createStyleTag.jsx';
 import DataStylesContext from '@/context/contex.js'
+import useBodyClass from '@/hooks/useBodyClass.js'
 
 const App = ()=>{
+  // Use context
   const context = useContext(DataStylesContext);
   const{elementorKit, elementorKitId, engineCSS, styleCSS} = context;
-    console.log('context from App', elementorKitId)
-    
-  // Initialises communication with the extension and injects the three style sheets
-  // useGetDataFromServiceWorker(); 
+  
+  // Add elementor kid Id class to body
+  useBodyClass(elementorKitId); 
 
   return(
     <>
