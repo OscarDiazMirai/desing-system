3f0f10317b58fdf6280197a2a60e2531b6f287ba
OscarDiazMirai <oscar.diaz@mirai.com>
2026-07-17 13:50:19 +0200

    refactor: improve logic fn

--- src/utils/splitVariable.js
+++ src/utils/splitVariable.js
@@ -1,6 +1,6 @@
 // Split a CSS variable into its name and colour code
 export const splitVariableCss = (variableString)=>{
-    if(!variableString) return;
+    if(!variableString) return { propertyName: '', value: '' };
     const[propertyName, value] = variableString.replace(';', '').split(':');
     return { propertyName, value };
 };
